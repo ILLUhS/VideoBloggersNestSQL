9771738ca033ff02bdbc805ef22927d676b21248
illuhs <illuhs@yandex.ru>
2023-03-21 19:34:56 +0300

    fix delete testing/all-data

--- src/api/controllers/testing.all.data.controller.ts
+++ src/api/controllers/testing.all.data.controller.ts
@@ -2,11 +2,15 @@ import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { SkipThrottle } from '@nestjs/throttler';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
+const tables = ['PasswordRecovery', 'RefreshTokenMetas', 'Users'];
 @SkipThrottle()
 @Controller('testing/all-data')
 export class TestingAllDataController {
   constructor(
+    @InjectDataSource() protected dataSource: DataSource,
     @InjectConnection()
     private readonly connection: Connection,
   ) {}
@@ -14,6 +18,9 @@ export class TestingAllDataController {
   @HttpCode(204)
   @Delete()
   async DeleteAll() {
+    for (const table of tables) {
+      await this.dataSource.query(`DELETE FROM "${table}"`);
+    }
     const collections = this.connection.collections;
     for (const key in collections) {
       await collections[key].deleteMany({});
